3d8fadb3cb18ab6e86de7c7d05e1a68392adacd8
ShahlaMirzayeva96 <49427424+ShahlaMirzayeva96@users.noreply.github.com>
2019-07-10 21:21:23 +0400

    Add files via upload

---
+++ 100719/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class LogInButton
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.passInput = new System.Windows.Forms.TextBox();
+            this.passCheck = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(490, 97);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(124, 134);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(212, 37);
+            this.txtInput.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 97);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Login:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password:";
+            // 
+            // passInput
+            // 
+            this.passInput.Location = new System.Drawing.Point(124, 247);
+            this.passInput.Name = "passInput";
+            this.passInput.Size = new System.Drawing.Size(206, 37);
+            this.passInput.TabIndex = 4;
+            this.passInput.UseSystemPasswordChar = true;
+            // 
+            // passCheck
+            // 
+            this.passCheck.AutoSize = true;
+            this.passCheck.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passCheck.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.passCheck.Location = new System.Drawing.Point(124, 292);
+            this.passCheck.Name = "passCheck";
+            this.passCheck.Size = new System.Drawing.Size(121, 22);
+            this.passCheck.TabIndex = 5;
+            this.passCheck.Text = "Show Password";
+            this.passCheck.UseVisualStyleBackColor = true;
+            this.passCheck.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(124, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Log In";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // LogInButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 441);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passCheck);
+            this.Controls.Add(this.passInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "LogInButton";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Checked";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox passInput;
+        private System.Windows.Forms.CheckBox passCheck;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 100719/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class LogInButton : Form
+    {
+        public LogInButton()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (passCheck.Checked)
+            {
+                passInput.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                passInput.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (txtInput.Text == "admin" && passInput.Text=="12345")
+            {
+                MessageBox.Show("Congratulations");
+            }
+            else if(txtInput.Text!="admin")
+            {
+                MessageBox.Show("Login is incorrect");
+            }else if (passInput.Text != "12345")
+            {
+                MessageBox.Show("Password is incorrect");
+            }
+        }
+    }
+}
